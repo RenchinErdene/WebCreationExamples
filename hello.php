e04ca3101f8485d8bd6d87aba732135a72f61ed1
RenchinErdene <ytrickgamer@gmail.com>
2025-11-12 13:02:57 +0800

    lab11 php

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<h1>Today is November 12th, 2025. Week 11</h1>
+</head>
+<?php
+	echo "X = 5<br>";
+	$x = 5;
+	echo "Y = 2<br>";
+	$y = 2;
+	echo "x * y =" . $x*$y;
+	echo "lab 9 php part 1"
+?>
+</html>
